9fec8c20c431a5ef162d6cf468d0326c14a5f3e7
Paulo <Paulovsaraty@gmail.com>
2022-06-22 13:15:25 -0300

    recreating repo
    the other one went to shit

---
+++ Assets/Bird.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+
+    public float speed = 1f;
+    private Rigidbody2D rigidbody;
+
+    public GameObject gameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            rigidbody.velocity = Vector2.up * speed;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        gameOver.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
