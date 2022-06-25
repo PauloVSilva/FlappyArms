a07da2be07ce825ea0d74ab14f2f98e9c5ce3ffa
Paulo <Paulovsaraty@gmail.com>
2022-06-25 17:37:00 -0300

    Added sounds and new background

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -2,25 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bird : MonoBehaviour
-{
+[RequireComponent(typeof(AudioSource))]
 
+public class Bird : MonoBehaviour{
     public float speed = 1f;
     private Rigidbody2D rigidbody;
-
     public GameObject gameOver;
+    AudioSource audioData;
+
+    public bool ceilingEnabled = true;
+    public bool floorProtection = false;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         rigidbody = GetComponent<Rigidbody2D>();
+        audioData = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+        if (ceilingEnabled){
+            if (rigidbody.transform.position.y >= 1) {
+                float xPos = rigidbody.transform.position.x;
+                Vector2 input = new Vector2(0, 0);
+
+                rigidbody.transform.position = new Vector2(xPos, 1);
+                rigidbody.velocity = input;
+            }
+        }
+
+        if (floorProtection){
+            if (rigidbody.transform.position.y <= -0.5f) {
+                float xPos = rigidbody.transform.position.x;
+                Vector2 input = new Vector2(0, 0);
+
+                rigidbody.transform.position = new Vector2(xPos, -0.5f);
+                rigidbody.velocity = input;
+            }
+        }
+
         if (Input.GetMouseButtonDown(0)){
             rigidbody.velocity = Vector2.up * speed;
+        }
+
+        if (rigidbody.velocity.y > 0){
+            if(!audioData.isPlaying){
+                audioData.Play();
+            }
+        }
+
+        if (rigidbody.velocity.y <= -0.85f){
+            audioData.Stop();
         }
     }
 
--- Assets/BirdMPU.cs
+++ Assets/BirdMPU.cs
@@ -10,13 +10,8 @@ public class BirdMPU : MonoBehaviour{// Start is called before the first frame u
     public string[] strData_recieved = new string[3];
     public int ax, ay, az;
 
-    public bool ceilingEnabled = true;
-    public bool floorProtection = false;
-
     public float speed = 1f;
     private Rigidbody2D rigidbody;
-
-    public GameObject gameOver;
     
     // Start is called before the first frame update
     void Start(){
@@ -28,30 +23,6 @@ public class BirdMPU : MonoBehaviour{// Start is called before the first frame u
     void Update(){
         strReceived = stream.ReadLine(); 
         strData = strReceived.Split(',');
-
-        if (ceilingEnabled){
-            if (rigidbody.transform.position.y >= 1) {
-                float xPos = rigidbody.transform.position.x;
-                Vector2 input = new Vector2(0, 0);
-
-                rigidbody.transform.position = new Vector2(xPos, 1);
-                rigidbody.velocity = input;
-            }
-        }
-
-        if (floorProtection){
-            if (rigidbody.transform.position.y <= -0.5f) {
-                float xPos = rigidbody.transform.position.x;
-                Vector2 input = new Vector2(0, 0);
-
-                rigidbody.transform.position = new Vector2(xPos, -0.5f);
-                rigidbody.velocity = input;
-            }
-        }
-
-        if (Input.GetMouseButtonDown(0)){
-            rigidbody.velocity = Vector2.up * speed;
-        }
 
         //this is where the magic happens I guess
         if (strData[0] != "" && strData[1] != "" && strData[2] != ""){
@@ -71,10 +42,5 @@ public class BirdMPU : MonoBehaviour{// Start is called before the first frame u
                 rigidbody.velocity = input;
             }
         }
-    }
-
-    void OnCollisionEnter2D(Collision2D collision){
-        gameOver.SetActive(true);
-        Time.timeScale = 0;
     }
 }
